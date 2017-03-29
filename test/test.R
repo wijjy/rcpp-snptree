febbb1491640ca9feb6acf9d36eb737817e4dac7
Ian Wilson <wilson.ianj@gmail.com>
2017-03-29 08:31:40 +0100

    Got a working version now

--- test/test.R
+++ test/test.R
@@ -1,9 +1,29 @@
 data("snptreeExample")
-a <- simple_split(haps, 5:ncol(haps))
+a <- simple_split(haps, position)
 nleaves(a)
 cases <- 1:1000
 case_control_leaves(a,cases)
-leaf_count(a)
+leaves <- leaf_count(a)
+sum(leaves)
+res <- get_coordinates(a, gap=100) 
+res[res[,1]==-1,1] <- max(res[,1])+1 
+plot(range(res[,1]), range(res[,2]), type="n", xlab="", ylab="")
+xspline(res[,1], res[,2], shape=res[,3], open=FALSE, col="lightgrey", border=FALSE)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -13,7 +33,6 @@ plot_stump <- function(x1, x2, y1, h1, y2, h2, gap) {
   s <- c(0,       0.5,     0,     0,   0.5,         0,         0,     0)
   plot(c(0,1), c(0,1), type = "n", axes = FALSE, xlab = "", ylab = "")
   xspline(x,y, shape=s, open=FALSE, repEnds=TRUE, col="lightgrey", border=FALSE)
-  
 }
 
 plot_stump(0.5, 1, 0.4, 0.2, 0.6, 0.15, 0.05)
